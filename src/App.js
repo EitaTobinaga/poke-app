857915120d986c03941b8075142bf61e2d2d0607
Eita <okomedayo@icloud.com>
2024-08-22 23:52:01 +0900

    ポケモンの画像を表示できるようにしました

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { useState } from 'react';
 function App() {
   const [name, setName] = useState('ポケモン');
   const [height, setHeight] = useState('ポケモンの大きさ');
+  const [frontImageUrl, setFrontImageUrl] = useState('');
+  const [backImageUrl, setBackImageUrl] = useState('');
+  const [sFrontImageUrl, setsFrontImageUrl] = useState('');
+  const [sBackImageUrl, setsBackImageUrl] = useState('');
+
   const randomId = Math.floor(Math.random() * 898) + 1
   async function getPokemon(){
     console.log(randomId)
@@ -14,8 +19,17 @@ function App() {
     const pokename = pokemon.data.name
     console.log(pokemon.data.height)
     const pokeheight = pokemon.data.height
-    setHeight(pokeheight)
+    console.log(pokemon.data.sprites.front_default)
+    const frontpokeimage = pokemon.data.sprites.front_default
+    const backpokeimage = pokemon.data.sprites.back_default
+    const sFrontpokeimage = pokemon.data.sprites.front_shiny
+    const sBackpokeimage = pokemon.data.sprites.back_shiny
     setName(pokename)
+    setHeight(pokeheight)
+    setFrontImageUrl(frontpokeimage)
+    setBackImageUrl(backpokeimage)
+    setsFrontImageUrl(sFrontpokeimage)
+    setsBackImageUrl(sBackpokeimage)
   }
 
 
@@ -32,6 +46,20 @@ function App() {
             <div className='ポケモン高さ用の土地'>
               <h3>ポケモンの高さ</h3>
               <p>{height}</p>
+            </div>
+            <div className='通常色ポケモンの画像説明'>
+              <h3>通常の姿</h3>
+            </div>
+            <div className='通常色ポケモンの画像'>
+              <img src={frontImageUrl} alt=''/>
+              <img src={backImageUrl} alt=''/>
+            </div>
+            <div className='色違いポケモンの画像説明'>
+              <h3>色違いの姿</h3>
+            </div>
+            <div className='色違いポケモンの画像'>
+              <img src={sFrontImageUrl} alt='色違い正面画像'/>
+              <img src={sBackImageUrl} alt='色違い背面画像'/>
             </div>
           <div>
             <button onClick={getPokemon}>ポケモン情報の取得</button>
